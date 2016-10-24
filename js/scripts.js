83e931cf073b2054a5c24bf8cb50ffbac3cf9f97
Cody Brubaker and Taylor Loftis-Kim <taylor@>
2016-10-24 10:39:50 -0700

    use DRY code with refactoring loops

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,15 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var nameInput = $("input#name").val();
-    var verbPresentInput = $("input#verbPresent").val();
-    var person1Input = $("input#person1").val();
-    var nounInput= $("input#noun").val();
-    var verbPastInput = $("input#verbPast").val();
-    var person2Input = $("input#person2").val();
-    var verbPresentTwoInput = $("input#verbPresentTwo").val();
-    var animalInput = $("input#animal").val();
+    event.preventDefault();
+    debugger;
 
-    $(".name").text(nameInput);
-    $(".verbPresent").text(verbPresentInput);
-    $(".person1").text(person1Input);
-    $(".noun").text(nounInput);
-    $(".verbPast").text(verbPastInput);
-    $(".person2").text(person2Input);
-    $(".verbPresentTwo").text(verbPresentTwoInput);
-    $(".animal").text(animalInput);
+    var blanks = ["name", "verbPresent", "person1", "noun", "verbPast", "person2", "verbPresentTwo", "animal"];
+
+    blanks.forEach(function(blank){
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
     $("#story").show();
-
-    event.preventDefault();
   });
 });
